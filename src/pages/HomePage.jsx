da6bbbb9e43aebc9d13e73ed48445290b30e5104
saurabh <saurabhmehta1601@gmail.com>
2023-04-10 03:17:41 +0530

    update HomePage UI based on auth state

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '../components/Header'
+import { useAuthenticatedUser } from 'react-appwrite-auth-ui'
+import { Navigate } from 'react-router-dom'
+import Quotes from '../components/Quotes'
 
 const HomePage = () => {
-  return (
-    <div>
-      <Header /> 
+  const {user,loading,error} = useAuthenticatedUser()
+
+  if(loading){
+    return <div className='loading__page'>
+      <div className="loading__section">
+        <span className="loading__spinner"></span>
+        <h3 className='loading__text'>Loading...</h3>
+      </div>
     </div>
-  )
+  }
+
+  if(error){
+    return <Navigate to='/sign-in' />
+  }
+
+  return (<div>
+    <Header /> 
+    {!user ? 
+      <section className='unauthorized__section'>You must login to see quotes</section> :
+      <Quotes />
+    }
+  </div>) 
 }
+
 
 export default HomePage
--- src/positiveQuotes.js
+++ src/positiveQuotes.js
@@ -1,42 +1,42 @@
 export default [
   {
     id: '1',
-    quote: 'The best way to predict the future is to create it.',
+    text: 'The best way to predict the future is to create it.',
   },
   {
     id: '2',
-    quote: 'The best revenge is massive success.',
+    text: 'The best revenge is massive success.',
   },
   {
     id: '3',
-    quote: 'The only way to do great work is to love what you do.',
+    text: 'The only way to do great work is to love what you do.',
   },
   {
     id: '4',
-    quote: 'If you are working on something that you really care about, you don’t have to be pushed. The vision pulls you.',
+    text: 'If you are working on something that you really care about, you don’t have to be pushed. The vision pulls you.',
   },
   {
     id: '5',
-    quote: 'People who are crazy enough to think they can change the world, are the ones who do.',
+    text: 'People who are crazy enough to think they can change the world, are the ones who do.',
   },
   {
     id: '6',
-    quote: 'Failure will never overtake me if my determination to succeed is strong enough.',
+    text: 'Failure will never overtake me if my determination to succeed is strong enough.',
   },
   {
     id: '7',
-    quote: 'Entrepreneurs are great at dealing with uncertainty and also very good at minimizing risk. That’s the classic entrepreneur.',
+    text: 'Entrepreneurs are great at dealing with uncertainty and also very good at minimizing risk. That’s the classic entrepreneur.',
   },
   {
     id: '8',
-    quote: 'We may encounter many defeats but we must not be defeated.',
+    text: 'We may encounter many defeats but we must not be defeated.',
   },
   {
     id: '9',
-    quote: 'Knowing is not enough; we must apply. Wishing is not enough; we must do.',
+    text: 'Knowing is not enough; we must apply. Wishing is not enough; we must do.',
   },
   {
     id: '10',
-    quote: 'Imagine your life is perfect in every respect; what would it look like?',
+    text: 'Imagine your life is perfect in every respect; what would it look like?',
   },
 ]
