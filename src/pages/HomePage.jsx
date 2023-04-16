fdd64f13dfc54bf43fdcce6e3fa2cd6b07228a7b
Saurabh <57309205+saurabhmehta1601@users.noreply.github.com>
2023-04-16 20:17:32 +0530

    update README

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Header from '../components/Header'
 import { useAuthenticatedUser } from 'react-appwrite-auth-ui'
 import { Navigate } from 'react-router-dom'
